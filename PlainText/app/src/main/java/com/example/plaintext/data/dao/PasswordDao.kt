77821614a7403f12609936312dc922731c111a7f
denis.guimaraes <denis.guimaraes@fpf.br>
2026-06-10 11:27:23 -0400

    feat: modeling password table

--- PlainText/app/src/main/java/com/example/plaintext/data/dao/PasswordDao.kt
+++ PlainText/app/src/main/java/com/example/plaintext/data/dao/PasswordDao.kt
@@ -1,8 +1,16 @@
 package com.example.plaintext.data.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import com.example.plaintext.data.model.Password
 
 @Dao
 abstract class PasswordDao : BaseDao<Password> {
+
+    @Query("""
+        SELECT * FROM passwords WHERE name = :name
+    """)
+    abstract fun getByName(name: String): Flow<PasswordDao>
+
 }
